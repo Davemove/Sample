695fc41a22cdecd2195070d1da2664a8ef180239
Davemove <princeadeyemo42@gmail.com>
2020-02-26 14:02:59 +0100

    php1

---
+++ cli.php
@@ -0,0 +1,57 @@
+<?php
+/*
+Author:David adeyemo
+Date:25/02/2020
+ */
+$fristname="David";//declare your name here
+$lastname="Adeyemo";
+$YOB=1996;
+$person['height']= "short";
+$person['complex']="fair";
+$person['nationality']="Ghana";
+$person1['name']="Joy";
+$person1['height']="tall";
+$person1['complex']="dark";
+$person1['nationality']="Nigerian";
+$person2['name']="kate";
+$person2['height']="tall";
+$person2['complex']="fair";
+$person2['nationality']="American";
+$person3['name']="Daniel";
+$person3['height']="long";
+$person3['complex']='yellow';
+$perosn3['nationality']="Nigerian";
+$person4['name']="Angel";
+$person4['height']="small";
+$person4['complex']="fair";
+$person4['nationality']="Ghanian";
+$person5['name']="samuel";
+$person5['height']="tall";
+$person5['complex']="black";
+$person5['nationality']="spain";
+$person6['name']="beliver";
+$person6['height']="tall";
+$person6['complex']="fair";
+$person6['nationality']="German";
+$person7['name']="Dayo";
+$person7['height']="tall";
+$person7['complex']="fair";
+$person7['nationality']"Nigerian";
+$person8['name']="Devil";
+$person8['height']="tall";
+$person8['complex']="fair";
+$person8['nationality']="congo";
+$person9['name']="mover";
+$person9['height']="dark";
+$person9['complex']="fair";
+$person9['nationality']="nigerian";
+$person10['name']="GOAT";
+$person10['height']="longer";
+$person10['complex']="fair";
+$person10['nationality']="yaba";
+
+
+print 'My name is '.$fristname.' '.$lastname. '\n';//print your name here
+echo "I was born in $YOB ,and i am 2020- $YOB year old";
+echo "i am a $person[height] and $person[complex] living in $person[nationality]";
+?>
